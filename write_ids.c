aaa8e812df2989b46b6af7a079b616b4189010d9
janit5511 <janitbhardwaj5@gmail.com>
2024-09-26 18:29:34 -0400

    Update write_ids.c

--- write_ids.c
+++ write_ids.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 int main() {
-    char GITHUB_ID[] = "Fill_in_Your_Github_Id";
-    char STUDENT_ID[] = "Fill_in_Your_Student_Id";
+    char GITHUB_ID[] = "janit5511";
+    char STUDENT_ID[] = "919341556";
 
     return 0 ;
 }
